7f9bc59d62d3e30dfc5d7de48d38aa94309ccb70
rony171998 <rony171998@gmail.com>
2022-05-03 13:06:49 -0500

    correccion de warning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 import ChangeColorBackground from "./ChangeColorBackground"
 import QuoteBox from "./QuoteBox"
 
-
 function App() {
 
-  
   return (
     <div >
       <QuoteBox/>
